ea0c4efe039103f7ed325777b2f38d6454fc0443
Innocentadika <90698819+Innocentadika@users.noreply.github.com>
2021-10-24 10:45:43 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+  Project: Library system
+  Author:Innocent Mukovwa
+  Date:Oct,2021
+  Compiler:GCC
+  Language:C99
+  License:MIT
+  Publisher:Longhorn
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("MENU\n");
+    printf("Add user\n");
+    printf("Add book\n");
+    printf("Book search\n");
+    printf("Book download\n");
+    printf("Edit profile\n");
+    printf("Add comment\n");
+    printf("User exit\n");
+
+    printf("Hello world!\n");
+    return 0;
+}
